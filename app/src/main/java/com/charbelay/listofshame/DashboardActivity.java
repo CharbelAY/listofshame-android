5ee26bf0984fcaea87bb322b1360afe330ef2183
Charbelay <charbel@abouyounes.com>
2019-04-19 22:17:19 +0300

    added fragments and navigation working

--- app/src/main/java/com/charbelay/listofshame/DashboardActivity.java
+++ app/src/main/java/com/charbelay/listofshame/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.charbelay.listofshame;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,7 +13,36 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment  = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_list:
+                            selectedFragment = new ListFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace
+                            (R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
 
     @Override
     public void onBackPressed() {
